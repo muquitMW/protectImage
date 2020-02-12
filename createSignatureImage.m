2c49ced82238f6cb3e208407c05c1983216d8f16
muquitMW <60499745+muquitMW@users.noreply.github.com>
2020-02-12 16:25:06 +0900

    Update createSignatureImage.m

--- createSignatureImage.m
+++ createSignatureImage.m
@@ -23,31 +23,4 @@ In      = insertObjectAnnotation(bI,...
 In      = rgb2gray(In);%Gray image
 In      = In >=1;%
 In      = cast(In,'like',I);%Adjust to input image's type
-
-%% Inputs
-% signText = 'Mohammad Muquit';
-% fontsize = 24;
-
-% %%
-% fgH = figure('Visible', 'off');
-% axes('Parent', fgH, 'xticklabel', [], 'yticklabel',[]);
-% text(0.1, 0.1, signText, 'Fontsize', fontsize);
-% 
-% %% 
-% I = getframe;
-% close(fgH);
-% I = I.cdata(:,:,1);
-% I = I <= 0;
-% 
-% %%
-% szI = size(I);
-% I = I(1:end - round(szI(1)*0.05), round(szI(2)*0.05):end);
-% 
-% %%
-% st = regionprops(I, 'PixelList');
-% lx = st(1).PixelList(:,1);
-% rx = st(end).PixelList(:,1);
-% ly = st(1).PixelList(:,2);
-% 
-% %%
-% In = I(max(min(ly) - 3,1) : min(max(ly) + 3,szI(1)), max(min(lx) - 3,1) : min(max(rx) + 3,szI(2)));
+end

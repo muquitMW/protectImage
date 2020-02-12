7704df70a0693d1c108999c0f8bff4f9b3fe15b1
muquitMW <60499745+muquitMW@users.noreply.github.com>
2020-02-12 16:17:24 +0900

    Update implantSignatureIntoImage.m

--- implantSignatureIntoImage.m
+++ implantSignatureIntoImage.m
@@ -1,13 +1,23 @@
 function I = implantSignatureIntoImage(I, sigI)
+%This function combines original image and signature images together
+%Inputs
+% I : original image
+% sigI: signature image
+%Outputs
+%I : combined image
+
+% For color image, the signature is put into BLUE image
 if ~ismatrix(I)
     Itmp        = I(:,:,3);
 else
     Itmp        = I;
 end
 
+% Deducts 1 from the lowest bit from input image
 ind         = mod(Itmp,2)>0;
-Itmp(ind)   = Itmp(ind) - 1;% Deduct 1 from the lowest bit
+Itmp(ind)   = Itmp(ind) - 1;
 
+% Adds signature image to input image
 if ~ismatrix(I)
     I(:,:,3)    = Itmp + sigI;
 else
